51ede506d261f2f69ae15baa12db4c21f82e28ca
ImRyan123 <24ayersr@mountainview.school.nz>
2026-03-24 13:19:40 +1300

    Added new files.

--- blogger/db.py
+++ blogger/db.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import click
 from flask import current_app, g
 
-
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
@@ -12,9 +11,7 @@ def get_db():
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
-
     return g.db
-
 
 def close_db(e=None):
     db = g.pop('db', None)
@@ -27,14 +24,12 @@ def init_db():
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
-
-
+        
 @click.command('init-db')
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
-
 
 sqlite3.register_converter(
     "timestamp", lambda v: datetime.fromisoformat(v.decode())
--- personal/test1.py
+++
@@ -1,8 +0,0 @@
-def count():
-    c = 1
-    while True:
-        for i in range(c):
-            print(", ".join([str(i + 1) for i in range(c)]))
-        c += 1
-
-count()
